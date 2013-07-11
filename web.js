b506b02e3b2a9e9277cc5f25d05009fd03e52659
John Smith <example@stanford.edu>
2013-07-11 19:41:02 +0000

    Added web.js

--- web.js
+++ web.js
@@ -10,3 +10,6 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+var buffer = fs.readFileSync('./index.html','utf8');
+buffer.toString();
+
